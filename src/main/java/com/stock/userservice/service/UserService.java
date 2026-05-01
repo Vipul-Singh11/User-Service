2290f78470800295364e518b1688e0407389548b
Vipul-Singh11 <itsvipulsingh11@gmail.com>
2026-05-02 00:01:55 +0530

    Add credit and debit methods to UserService interface

--- src/main/java/com/stock/userservice/service/UserService.java
+++ src/main/java/com/stock/userservice/service/UserService.java
@@ -2,9 +2,15 @@ package com.stock.userservice.service;
 
 import com.stock.userservice.dto.UserResponse;
 
+import java.math.BigDecimal;
+
 public interface UserService {
 
     UserResponse getCurrentUser();
 
     UserResponse getUserById(Long id);
+
+    void credit(Long userId, BigDecimal amount);
+
+    void debit(Long userId, BigDecimal amount);
 }
